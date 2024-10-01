99889e79fe04a36da65d07c46206d16f2227daf6
emmanuel.mutua <emmanuel.mutua@turnkeyafrica.com>
2024-10-01 07:35:33 +0300

    add login and sign up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Login from "./login/login";
+import SignUp from "./signup/SignUp";
 
 export default function Home() {
   return (
     <div>
      <Login />
+     {/* <SignUp /> */}
     </div>
   );
 }
